fc209e0b10760a4516c7c680b681b4f3f3509c0a
MrGanesh <ganeshkulkarni689@gmail.com>
2021-04-28 12:59:40 +0530

    3 files modified, 1 added, 1 deleted

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import GlobalStyle from './styles/GlobalStyle'
 import App from "./App";
+import {BrowserRouter} from 'react-router-dom'
+
 
 ReactDOM.render(
   <>
+  <BrowserRouter>
 <App />
+</BrowserRouter>
 <GlobalStyle />
 </>
 , document.getElementById("root"));
